2afece709df42d27a82a111ac132a124476d09b0
Roland Koimans <roland.koimans@gmail.com>
2021-09-24 20:05:24 +0200

    Input

--- TreeSolver/TreeSolver/Program.cs
+++ TreeSolver/TreeSolver/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TreeSolver
 {
@@ -7,7 +8,27 @@ namespace TreeSolver
         // THIS IS A SOLUTION FOR THE OFFLINE VARIANT OF THE PRACTICAL
         static void Main(string[] args)
         {
-           // Handle all input
+            // Handle all input
+            int firstDoseTime = Int32.Parse(Console.ReadLine());
+            int secondDoseTime = Int32.Parse(Console.ReadLine());
+            int gapTime = Int32.Parse(Console.ReadLine());
+            int patientAmount = Int32.Parse(Console.ReadLine());
+
+            // Reading all patients
+            List<Patient> patients = new List<Patient>();
+            for (int i = 0; i < patientAmount; i++)
+            {
+                // Separate values
+                string[] patientline = Console.ReadLine().Split(',');
+                
+                // Remove whitespace
+                foreach(string str in patientline)
+                {
+                    str.Trim();
+                }
+
+                patients.Add(new Patient(Int32.Parse(patientline[0]), Int32.Parse(patientline[1]), Int32.Parse(patientline[2]), Int32.Parse(patientline[3])));
+            }
 
 
         }
