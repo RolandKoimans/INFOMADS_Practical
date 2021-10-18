e61f61284d6f4b4d9d5b07e0ba2f34fc4ecbc3ac
Roland Koimans <roland.koimans@gmail.com>
2021-10-18 13:38:14 +0200

    Init Online Algorithm

---
+++ OnlineScheduling/OnlineScheduling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnlineScheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
